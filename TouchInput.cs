d4b2729696aad1e178ac3b255a8c1abf26322785
torecan <torecancelik@gmail.com>
2017-08-20 20:44:16 +0200

    Add files via upload

---
+++ TouchInput.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+    public LayerMask touchInp;
+
+    private List<GameObject> touchList = new List<GameObject>();
+    private GameObject[] touchesOld;
+    RaycastHit hit;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+        {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            foreach (Touch touch in Input.touches)
+            {
+
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, touchInp))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+                    if (Input.GetMouseButton(0))
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                }
+
+            }
+                foreach (GameObject g in touchesOld)
+                {
+                    if (!touchList.Contains(g))
+                    {
+                        g.SendMessage("onTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+
+            
+        }
+
+
+#endif
+
+        if (Input.touchCount > 0)
+        {
+
+            touchesOld = new GameObject[touchList.Count];
+            touchList.CopyTo(touchesOld);
+            touchList.Clear();
+
+            foreach (Touch touch in Input.touches)
+            {
+
+                Ray ray = GetComponent<Camera>().ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, touchInp))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Ended)
+                    {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+
+                }
+
+
+                foreach (GameObject g in touchesOld)
+                {
+                    if (!touchList.Contains(g))
+                    {
+                        g.SendMessage("onTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+    //  public Color color;
+    public Material[] mat;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        
+    }
+    // Update is called once per frame
+    void OnTouchDown()
+    {
+        rend.sharedMaterial = mat[0];
+    }
+
+    void OnTouchUp()
+    {
+        rend.sharedMaterial = mat[1];
+    }
+
+    void onTouchExit()
+    {
+        rend.sharedMaterial = mat[2];
+    }
+
+
+}
